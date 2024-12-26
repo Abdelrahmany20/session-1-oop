9b61b188c9accaa92af365ce783c5cacba3d3868
Abdelrahmany20 <abdelrahman.yasser.m17@gmail.com>
2024-12-26 05:37:45 +0200

    Add project files.

---
+++ session 1 oop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace session_1_oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
