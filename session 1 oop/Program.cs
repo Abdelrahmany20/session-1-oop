99a22e90cb186a53af494318aca273b88db36464
Abdelrahmany20 <abdelrahman.yasser.m17@gmail.com>
2024-12-27 18:31:38 +0200

    Demo

--- session 1 oop/Program.cs
+++ session 1 oop/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Drawing;
 using System.Reflection;
 
@@ -9,10 +10,217 @@ namespace session_1_oop
         static void Main(string[] args)
         {
 
+            #region Exceptions Handle
+
+            // Exceptions
+            // 1. System Exceptions
+            //// 1.1 Format Exception
+            //// 1.2 Index Out Of Range Exception
+            //// 1.3 Null Reference Exception
+            //// 1.4 Invalid Operation Exception
+            //1.5 Arithmetic Exception
+            //1.5.1 Divided By Zero Exception
+            //1.5.2 Overflow Exception
+            // 2. Application Exception
 
 
 
 
+
+
+
+            //public static void DoSomeCode()
+
+            //int X, Y, Z;
+            //X = int. Parse(Console.ReadLine());// FormatException
+            //Y = int. Parse(Console.ReadLine());
+
+            //Z = X / Y;// DivideByZeroException
+
+            //int[] Numbers = { 1, 2, 3 };
+
+            //Numbers[10] = 100;// IndexOutOfRangeException
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+            //try
+            //{
+            //int X, Y, Z;
+            //bool Flag;
+            ////X= int.Parse(Console. ReadLine());// FormatException
+            //do
+
+            //{
+            //Flag = int.TryParse(Console.ReadLine(), out X);
+            //} while (!Flag) ;
+            //Y = int.Parse(Console.ReadLine());
+
+            //Z = X / Y;// DivideByZeroException
+
+
+            //if (Numbers?.Length > 10)
+            //{
+            ////int[] Numbers = { 1, 2, 3 };
+
+            //}
+
+            //Numbers[10] = 100;// IndexOutOfRangeException
+            //}
+
+
+            //try
+            //{
+
+            //    DoSomeProtectiveCode();
+            //}
+            //catch
+            //{
+            //    (Exception ex)
+            //}
+            //Console.WriteLine(ex.Message);
+
+
+
+
+            //            try
+            //            {
+            //                DoSomeProtectiveCode();
+            //                throw new Exception();
+            //            }}
+            //            catch { (Exception ex)
+
+            //Console.WriteLine(ex.Message);
+            //    }
+            //finally
+            //    {  
+            //Console.WriteLine("finally    ");
+            //// CLose - Free - Delete - Dealocate Unmanged Recources
+            //// Open File
+            //// Open Connection Database
+            //}}
+            #endregion
+
+
+            #region Asscess Modifiers
+
+            // private --> inside class only
+            // internal --> inside class scope and other classes inside same project
+            // internal --> inside class scope and other classes inside other project (Import)
+
+
+            //public TypeA()
+            //{
+            //TypeB typeB = new TypeB();
+            //}
+            //prIvate int X;
+            //internal int Y;
+            //public int Z;
+
+
+            //            internal interface IType
+
+            //What You Can Write Inside The Interface?
+            //1. Signature For Property => C# 7.0
+            //2. Signature For Method => C# 7.0
+            //3. Default Implemented Method => C# 8.0 Feature .Net Core 3.1 [2019]
+            #endregion
+
+            #region ENUM
+
+
+
+            //  enum Gender
+            //  {
+            //Male , Female
+            // }
+            //enum Days
+            //  {
+            //Saturday,
+            //Sunday,
+            //Monday,
+            //Tuesdayl,
+            //Wednesday,
+            //Thursday,
+            //Friday
+            //   }
+
+            //    enum Gender : byte
+            //{
+            //    Male, Female
+            //}
+            //enum Grades
+            //{
+            //    A, B, C, D, E, F
+            //}
+
+            //    enum Roles
+            //{
+            //    Admin = 10, Viewer = 20, Editor = 30
+            //}
+
+
+
+
+            #region Ex01
+
+            //string Day = "Hamada";
+            //Days Day = Days. Friday;
+            //   Grades grade = Grades.A;
+            //    if (grade == Grades.A)
+            //{
+            //        Console.WriteLine(":)");
+            // }
+            //      else
+            // {
+            //           Console.WriteLine(":(");
+            //           }
+
+            #endregion
+
+            #region Ex02
+
+            //Grades grade = (Grades)2;
+
+            //Console.WriteLine(grade);
+
+
+            //Console.WriteLine(grade); // C
+            //Grades grade = (Grades)10;// Excplicit Casting
+
+            //Console.WriteLine(grade); // }0
+
+            #endregion
+
+
+            #region Ex03
+            //Grades grade = (Grades) Enum. Parse(typeof(Grades), Console. ReadLine());
+            //EnumITryParse(typeof(Grades), Console. ReadLine(), out object Result);
+            //Grades grade = (Grades) Result;
+            //Console. WriteLine (Result);
+            #endregion
+
+
+            #region Ex04
+            //string gender = "male";
+
+            //Enum.TryParse(typeof(Gender), gender, out object Result);
+
+            //Console.WriteLine(Result);
+
+            //Enum.TryParse(gender, true, out Gender Result);
+            #endregion
+
+            #endregion
 
             #region permissions
 
